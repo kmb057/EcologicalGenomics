bbcf49f6714867e1105fd43ec7ee004f4937053d
kmb057 <kerriaburns9@gmail.com>
2020-02-05 11:35:17 -0500

    writing my mapping pipeline
    writing my mapping pipeline scraipt

---
+++ myscripts/mapping.sh
@@ -0,0 +1,41 @@
+>
+> do
+>   reverse=${forward/_R1.cl.pd.fq/_R2.cl.pd.fq}
+>   f=${forward/_R1.cl.pd.fq/}
+>   name=`basename ${f}`
+>   bwa mem -t 1 -M ${ref} ${forward} ${reverse} > ${output}/BWA/${name}.sam
+> done
+>
+> ^C
+[kburns@pbio381 ~]$ for forward in ${input}*_R1.cl.pd.fq
+>
+> do
+>   reverse=${forward/_R1.cl.pd.fq/_R2.cl.pd.fq}
+>   f=${forward/_R1.cl.pd.fq/}
+>   name=`basename ${f}`
+>   bwa mem -t 1 -M ${ref} ${forward} ${reverse} > ${output}/BWA/${name}.sam
+> done
+-bash: /BWA/XFS_01.sam: No such file or directory
+[kburns@pbio381 ~]$ vim
+do
+
+  out=${f/.sam/}
+  sambamba-0.7.1-linux-static view -S --format=bam ${f} -o ${out}.bam
+  samtools sort ${out}.bam -o ${out}.sorted.bam
+
+done
+
+#Now, let's remove the PCR duplicates
+for file in ${output}/BWA/${mypop}*.sorted.bam
+
+do
+  f=${file/.sorted.bam/}
+  sambamba-0.7.1-linux-static markdup -r -t 1 ${file} ${f}.sorted.rmdup
+done
+
+#Now, to finish we'll index our files
+for file in ${output}/BWA/${mypop}*.sorted.rmdup.bam
+
+do
+  samtools index ${file}
+done
---
+++ myscripts/mypipeline.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#We'll use this wrapper to run our different scripts.
+
+#Path to my repo
+
+myrepo="/users/k/b/kburns/EcologicalGenomics/"
+
+#My population
+mypop="XFS"
+
+#Directory to our cleaned and paired reads
+input="/data/project_data/RS_ExomeSeq/fastq/edge_fastq/pairedcleanreads/${mypop}"
+
+#Directory to store the outputs on our mapping
+output="/data/project_data/RS_ExomeSeq/mapping
+
+
+#My population
+mypop="XFS_01"
+
+#Directory to our cleaned and paired reads
+input="/data/project_data/RS_ExomeSeq/fastq/edge_fastq/pairedcleanreads/${mypop}"
+
+#Run mapping.sh
+source ./mapping.sh
+
+#Run the post-processing steps
+source ./process_bam.sh
